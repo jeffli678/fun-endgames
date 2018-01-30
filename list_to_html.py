5b49105a0f746aab7134167729eeeb7725ffffd3
jeff <jeffli678@sina.com>
2018-01-30 11:32:20 -0500

    add index in list

--- list_to_html.py
+++ list_to_html.py
@@ -5,13 +5,16 @@ endgame_list = open('endgame_list.txt').read().splitlines()
 endgame_html = open('endgame.part3.me/endgame.html', 'wb')
 endgame_private_html = open('endgame.part3.me/endgame_private.html', 'wb')
 
-for line in endgame_list:
-    # print(line)
-    endgame_html.write('<a href="http://www.chessdb.cn/query/?%s" target="_blank">%s</a><br />\n'\
-    % (line, line))
+for i in range(len(endgame_list)):
 
-    endgame_private_html.write('<a href="http://chess.frp.part3.me#%s" target="_blank">%s</a><br />\n'\
-    % (line, line)
+    line = endgame_list[i]
+    index = i + 1
+
+    endgame_html.write('%s. <a href="http://www.chessdb.cn/query/?%s" target="_blank">%s</a><br />\n'\
+    % (index, line, line))
+
+    endgame_private_html.write('%s. <a href="http://chess.frp.part3.me#%s" target="_blank">%s</a><br />\n'\
+    % (index, line, line)
     )
 
 endgame_html.close()
